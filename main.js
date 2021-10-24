d26b548b00e8822809a06eac9c5c88776427a363
Jhon Lima <67339976+J297-hub@users.noreply.github.com>
2021-10-23 23:00:24 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const LinksSocialMedia = {
+  github: 'J297-hub',
+  youtube: 'UCOzyDma0zIVTEBuaW1_5-dQ',
+  facebook: 'jhon.lima.967806',
+  instagram: 'jhonlima4162',
+  twitter: 'JhonLim17510592'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      UserImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
